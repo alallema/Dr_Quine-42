8f11392cc48326db8504fd83083964c92a9fc361
Amelie Lallemand <amlallemand@vente-privee.com>
2019-08-10 12:41:21 +0200

    Last ok

--- C/Sully.c
+++ C/Sully.c
@@ -1,14 +1,16 @@
 #include <stdlib.h>
+#include <unistd.h>
 #include <stdio.h>
-#define I 5
-#define STR(s) #s
-#define FILE_NAME(nb) "Sully_"STR(nb)".c"
-#define COMMAND "gcc "FILE_NAME(I)
-int main(){
-char *s = "#include <stdlib.h>%c#include <stdio.h>%c#define I %d%c#define STR(s) #s%c#define FILE_NAME(nb) %cSully_%cSTR(nb)%c.c%c%cint main(){%cchar *s = %c%s%c;%cFILE *fp;%cfp = fopen(FILE_NAME(I), %cw+%c);%c}";
+int i = 5;
+int main()
+{if (i > 0)
+{char filename[10];sprintf(filename, "Sully_%d.c", i);
+if (access(filename, F_OK ) != -1){i--;sprintf(filename, "Sully_%d.c", i);}
 FILE *fp;
-fp = fopen(FILE_NAME(I), "w+");
-system(COMMAND);
-fprintf(fp,s,10,10,I-1,10,10,34,34,34,34,10,10,34,s,34,10,10,34,34,10);
+sprintf(filename, "Sully_%d.c", i);
+fp = fopen(filename, "w+");
+char *s = "#include <stdlib.h>%c#include <unistd.h>%c#include <stdio.h>%cint i = %d;%cint main()%c{if (i > 0)%c{char filename[10];sprintf(filename, %cSully_%cd.c%c, i);%cif (access(filename, F_OK ) != -1){i--;sprintf(filename, %cSully_%cd.c%c, i);}%cFILE *fp;%csprintf(filename, %cSully_%cd.c%c, i);%cfp = fopen(filename, %cw+%c);%cchar *s = %c%s%c;%cfprintf(fp,s,10,10,10,i,10,10,10,34,37,34,10,34,37,34,10,10,34,37,34,10,34,34,10,34,s,34,10,10,10,34,37,37,37,34,10);%cfclose(fp);%cchar command[50];sprintf(command,%cgcc %cs -o Sully_%cd && ./Sully_%cd%c,filename,i,i);%csystem(command);}}";
+fprintf(fp,s,10,10,10,i,10,10,10,34,37,34,10,34,37,34,10,10,34,37,34,10,34,34,10,34,s,34,10,10,10,34,37,37,37,34,10);
 fclose(fp);
-}
+char command[50];sprintf(command,"gcc %s -o Sully_%d && ./Sully_%d",filename,i,i);
+system(command);}}
