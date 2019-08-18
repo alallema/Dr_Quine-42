3516ebbcefd8f3b0ef235965d99013dfa15f1631
Amelie Lallemand <amlallemand@vente-privee.com>
2019-08-18 14:15:33 +0200

    little fix in syntax

--- JS/Colleen.js
+++ JS/Colleen.js
@@ -2,13 +2,14 @@
 Comment1
 */
 function bs(){return unescape("%2f");}
-(function main(){
+function main(){
 /*
 Comment2
 */
 q=unescape("%27");
 nl=unescape("%0a");
 star=unescape("%2a");
-s='%s%s%sComment1%s%s%s%sfunction bs(){return unescape("%2f");}%s(function main(){%s%s%s%sComment2%s%s%s%sq=unescape("%27");%snl=unescape("%0a");%ss=%s%s%s;%sconsole.log(s,bs(),bs(),nl,nl,nl,bs(),bs(),nl,nl,nl,q,s,q,nl,nl)%s})()';
-console.log(s,bs(),star,nl,nl,star,bs(),nl,nl,nl,star,bs(),nl,nl,star,bs(),nl,nl,nl,q,s,q,nl,nl)
-})()
+s='%s%s%sComment1%s%s%s%sfunction bs(){return unescape("%2f");}%sfunction main(){%s%s%s%sComment2%s%s%s%sq=unescape("%27");%snl=unescape("%0a");%sstar=unescape("%2a");%ss=%s%s%s;%sconsole.log(s,bs(),star,nl,nl,star,bs(),nl,nl,nl,bs(),star,nl,nl,star,bs(),nl,nl,nl,nl,q,s,q,nl,nl,nl)%s}%smain()';
+console.log(s,bs(),star,nl,nl,star,bs(),nl,nl,nl,bs(),star,nl,nl,star,bs(),nl,nl,nl,nl,q,s,q,nl,nl,nl)
+}
+main()
